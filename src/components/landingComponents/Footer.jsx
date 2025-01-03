eb31c1dd8cd061886dc2b598f233a6ebddb6d7d1
PaarthAgarwal1 <paarthagarwal1@gmail.com>
2025-01-04 00:06:40 +0530

    improve the structure of about,landing and footer page

--- src/components/landingComponents/Footer.jsx
+++ src/components/landingComponents/Footer.jsx
@@ -1,117 +1,87 @@
-import { Link } from 'react-router-dom'
-import logo from '../../assets/logo/logoLight.svg'
-import insta from '../../assets/social/instagram.svg'
-import facebook from '../../assets/social/facebook.svg'
-import github from '../../assets/social/github.svg'
-import x from '../../assets/social/x.svg'
+import { Link } from "react-router-dom";
+import { FaInstagram, FaGithub, FaFacebookSquare } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import logo from "../../assets/logo/logoLight.svg"; 
 
-function Icons() {
-    return (
-        <div className="w-[150px] mt-3 ml-2 flex justify-between">
-            <figure className="w-6 h-6 hover:cursor-pointer">
-                <img src={insta} alt="Img" />
-            </figure>
-            <figure className="w-6 h-6 hover:cursor-pointer">
-                <img src={github} alt="Img" />
-            </figure>
-            <figure className="w-6 h-6 hover:cursor-pointer">
-                <img src={x} alt="Img" />
-            </figure>
-            <figure className="w-6 h-6 hover:cursor-pointer">
-                <img src={facebook} alt="Img" />
-            </figure>
+const socialLinks = [
+  { icon: <FaInstagram className="text-xl hover:text-white" />, url: "#" },
+  { icon: <FaGithub className="text-xl hover:text-white" />, url: "#" },
+  { icon: <FaXTwitter className="text-xl hover:text-white" />, url: "#" },
+  { icon: <FaFacebookSquare className="text-xl hover:text-white" />, url: "#" },
+];
+
+const footerLinks = [
+  {
+    heading: "LEARNIUM",
+    links: ["About", "Blog", "Careers", "Advertise"],
+  },
+  {
+    heading: "Support",
+    links: ["Contact", "Report an Issue"],
+  },
+  {
+    heading: "Policy",
+    links: ["Terms and Conditions", "Privacy", "Security"],
+  },
+];
+
+function SocialIcons() {
+  return (
+    <div className="flex space-x-5 mt-4">
+      {socialLinks.map(({ icon, url }, index) => (
+        <a
+          key={index}
+          href={url}
+          className="w-9 h-9 flex items-center justify-center rounded-full bg-gray-800 hover:bg-gray-700 transition-all duration-300 shadow-md hover:scale-110 text-gray-300"
+        >
+          {icon}
+        </a>
+      ))}
+    </div>
+  );
+}
+
+function FooterLinks() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 md:gap-12">
+      {footerLinks.map(({ heading, links }, index) => (
+        <div key={index}>
+          <h3 className="text-lg font-semibold text-gray-100 mb-3">{heading}</h3>
+          <ul className="space-y-2">
+            {links.map((link, idx) => (
+              <li key={idx}>
+                <a
+                  href="#"
+                  className="text-gray-400 hover:text-gray-100 transition-colors duration-200 block"
+                >
+                  {link}
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-    )
-}
-
-function FooterRight() {
-    const data = [
-        {
-            heading: "LEARNIUM",
-            linkList: [
-                { linkName: "About", link: "#" },
-                { linkName: "Blog", link: "#" },
-                { linkName: "Careers", link: "#" },
-                { linkName: "Advertize", link: "#" },
-            ]
-        },
-        {
-            heading: "Support",
-            linkList: [
-                { linkName: "Contact", link: "#" },
-                { linkName: "Report a issue", link: "#" }
-            ]
-        },
-        {
-            heading: "Policy",
-            linkList: [
-                { linkName: "Terms and Condition", link: "#" },
-                { linkName: "Privacy", link: "#" },
-                { linkName: "Security", link: "#" }
-            ]
-        },
-    ]
-
-    return (
-        <main className="w-2/4 flex flex-col md:flex-row justify-between">
-            <FooterRightComponent data={data} />
-        </main>
-    )
-}
-
-function FooterRightComponent({ data }) {
-    const rightComponents = data.map(ele =>
-        <div key={data.indexOf(ele)} className="w-[8rem] sm:w-40 mx-4 flex flex-col">
-            <h3 className="text-xl mb-2 mt-5">{ele.heading}</h3>
-            <hr />
-            <ul>
-                <RightComponentLinks linkList={ele.linkList} />
-            </ul>
-        </div>
-    )
-
-    return (<>
-        {rightComponents}
-    </>
-    )
-}
-
-function RightComponentLinks({ linkList }) {
-    const links = linkList.map(e => {
-        return (
-            <li key={linkList.indexOf(e)} className="mt-1 hover:underline">
-                <a href={e.link}>{e.linkName}</a>
-            </li>
-        )
-    })
-    return (<>
-        {links}
-    </>
-    )
+      ))}
+    </div>
+  );
 }
 
 function Footer() {
-    return (
-        <div className='bg-customBlack/90'>
-            <div className="container w-[82vw] mx-auto flex flex-col text-customCream">
-                <div className="min-h-[300px] flex flex-col md:flex-row justify-evenly items-center overflow-hidden">
-                    <aside className='sm:mr-10'>
-                        <Link to="/">
-                            <div className="w-40 mt-5 mx-1 hover:cursor-pointer">
-                                <img src={logo} alt="" />
-                            </div>
-                        </Link>
-                        <Icons />
-                    </aside>
-                    <FooterRight />
-                </div>
-                <section className="h-[80px] mt-5">
-                    <hr />
-                    <div className="mt-2">&copy; 2024 · LLC · All rights reserved</div>
-                </section>
-            </div>
+  return (
+    <footer className="bg-gray-900 text-gray-300 py-12">
+      <div className="container mx-auto px-6 md:px-12 flex flex-col md:flex-row items-center md:items-start justify-between">
+        <div className="text-center md:text-left">
+          <Link to="/">
+            <img src={logo} alt="LearnUI Logo" className="w-40 mb-4 mx-auto md:mx-0" />
+          </Link>
+          <SocialIcons />
         </div>
-    )
+        <FooterLinks />
+      </div>
+      <div className="border-t border-gray-700 mt-10 pt-5 text-center text-sm text-gray-400">
+        &copy; 2024 Learnium. All rights reserved.
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
